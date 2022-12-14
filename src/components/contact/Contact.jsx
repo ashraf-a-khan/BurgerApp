6ad2f39d5cd7b104481529e3707fb4ecca1d74e6
ashraf-a-khan <khan.ashraf2311@gmail.com>
2022-12-13 19:21:28 -0500

    Add contact page and style

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,7 +5,19 @@ import burger from "../../assets/burger2.png";
 const Contact = () => {
   return (
     <section className="contact">
-      <motion.form>
+      <motion.form
+        initial={{
+          x: "-100vw",
+          opacity: 0,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+        }}
+        transition={{
+          delay: 0.2,
+        }}
+      >
         <h2>Contact Us</h2>
         <input type="text" placeholder="Name" />
         <input type="email" placeholder="Email" />
@@ -15,8 +27,35 @@ const Contact = () => {
         <button type="submit">Send</button>
       </motion.form>
 
-      <motion.div className="formBorder">
-        <motion.div>
+      <motion.div
+        className="formBorder"
+        initial={{
+          x: "100vw",
+          opacity: 0,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+        }}
+        transition={{
+          delay: 0.2,
+        }}
+      >
+        <motion.div
+          initial={{
+            y: "-100vh",
+            x: "50%",
+            opacity: 0,
+          }}
+          animate={{
+            y: "-50%",
+            x: "50%",
+            opacity: 1,
+          }}
+          transition={{
+            delay: 1,
+          }}
+        >
           <img src={burger} alt="Burger" />
         </motion.div>
       </motion.div>
